e011bb42cd59550494e17c9ea40e7cf2d4a9d147
freedom909 <gifu0@outlook.com>
2023-09-09 16:14:22 +0900

    modified seeders

--- services/listings/migrations/20210921154100-create-listing-and-amenities.js
+++ services/listings/migrations/20210921154100-create-listing-and-amenities.js
@@ -1,70 +1,72 @@
 'use strict';
-export async function up(queryInterface, Sequelize) {
-  await queryInterface.createTable('Amenities', {
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: Sequelize.STRING,
-    },
-    category: {
-      type: Sequelize.STRING,
-    },
-    name: {
-      type: Sequelize.STRING,
-    },
-  });
-  await queryInterface.createTable('Listings', {
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: Sequelize.STRING,
-    },
-    title: {
-      type: Sequelize.STRING,
-    },
-    description: {
-      type: Sequelize.STRING,
-    },
-    costPerNight: {
-      type: Sequelize.FLOAT,
-    },
-    hostId: {
-      type: Sequelize.STRING,
-    },
-    locationType: {
-      type: Sequelize.STRING,
-    },
-    numOfBeds: {
-      type: Sequelize.INTEGER,
-    },
-    photoThumbnail: {
-      type: Sequelize.STRING,
-    },
-    isFeatured: {
-      type: Sequelize.BOOLEAN,
-    },
-  });
-  await queryInterface.createTable('ListingAmenities', {
-    ListingId: {
-      allowNull: false,
-      type: Sequelize.STRING,
-      references: {
-        model: 'Listings',
-        key: 'id',
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Amenities', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
       },
-    },
-    AmenityId: {
-      allowNull: false,
-      type: Sequelize.STRING,
-      references: {
-        model: 'Amenities',
-        key: 'id',
+      category: {
+        type: Sequelize.STRING,
       },
-    },
-  });
-}
-export async function down(queryInterface, Sequelize) {
-  await queryInterface.dropTable('ListingAmenities');
-  await queryInterface.dropTable('Listings');
-  await queryInterface.dropTable('Amenities');
-}
+      name: {
+        type: Sequelize.STRING,
+      },
+    });
+    await queryInterface.createTable('Listings', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      costPerNight: {
+        type: Sequelize.FLOAT,
+      },
+      hostId: {
+        type: Sequelize.STRING,
+      },
+      locationType: {
+        type: Sequelize.STRING,
+      },
+      numOfBeds: {
+        type: Sequelize.INTEGER,
+      },
+      photoThumbnail: {
+        type: Sequelize.STRING,
+      },
+      isFeatured: {
+        type: Sequelize.BOOLEAN,
+      },
+    });
+    await queryInterface.createTable('ListingAmenities', {
+      ListingId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        references: {
+          model: 'Listings',
+          key: 'id',
+        },
+      },
+      AmenityId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        references: {
+          model: 'Amenities',
+          key: 'id',
+        },
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ListingAmenities');
+    await queryInterface.dropTable('Listings');
+    await queryInterface.dropTable('Amenities');
+  },
+};
