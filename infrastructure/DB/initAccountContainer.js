c994799ada263f7587941e748371d88ccd26f59e
freedom909 <gifu0@outlook.com>
2024-08-02 13:31:31 +0900

    refactor initAccountContainer.js

--- infrastructure/DB/initAccountContainer.js
+++ infrastructure/DB/initAccountContainer.js
@@ -6,6 +6,9 @@ import AccountService from '../services/accountService.js';
 import AccountRepository from '../repositories/accountRepository.js';
 import UserService from '../services/userService.js';
 import UserRepository from '../repositories/userRepository.js';
+import connectMysql from './connectMysqlDB.js';
+import ListingRepository from '../repositories/listingRepository.js';
+import CartRepository from '../repositories/cartRepository.js';
 const initAccountContainer = async ({ services = [] } = {}) => {
   let mongodb;
 
@@ -17,15 +20,27 @@ const initAccountContainer = async ({ services = [] } = {}) => {
     console.error('Error connecting to MongoDB database:', error);
     throw error;
   }
+  let mysqldb
+  try {
+    mysqldb=await connectMysql()
+    console.log('Connected to MongoDB database');
+  } catch (error) {
+    console.error('Error connecting to MysqlDB database:', error);
+    throw error;
+  
+  }
 
   // Create a container and register services and repositories
   const container = createContainer();
   container.register({
     mongodb: asValue(mongodb),
+    mysqldb: asValue(mysqldb),
     userRepository: asClass(UserRepository).singleton(),
     userService: asClass(UserService).singleton(),
     accountService: asClass(AccountService).singleton(),
-    accountRepository: asClass(AccountRepository).singleton(),  
+    accountRepository: asClass(AccountRepository).singleton(),
+    listingRepository: asClass(ListingRepository).singleton(),  // Ensure this is registered
+    cartRepository: asClass(CartRepository).singleton(),
   });
   return container;
 };
--- subgraph-accounts/index.js
+++ subgraph-accounts/index.js
@@ -7,12 +7,6 @@ import { readFileSync } from 'fs';
 import http from 'http';
 import express from 'express';
 import cors from 'cors';
-// import initMongoContainer from '../infrastructure/DB/initMongoContainer.js';
-// import initListingContainer from '../infrastructure/DB/initListingContainer.js';
-// import initCartContainer from '../infrastructure/DB/initCartContainer.js';
-// import initReviewContainer from '../infrastructure/DB/initReviewContainer.js';
-// import initProfileContainer from '../infrastructure/DB/initProfileContainer.js';
-// import initBookingContainer from '../infrastructure/DB/initBookingContainer.js';
 import initAccountContainer  from '../infrastructure/DB/initAccountContainer.js';
 import resolvers from './resolvers.js';
 
@@ -75,33 +69,4 @@ const startApolloServer = async () => {
 
 startApolloServer();
 
-// import { ApolloServer } from '@apollo/server';
-
-// import { gql } from 'graphql-tag';
-// import { readFileSync } from 'fs';
-// import http from 'http';
-// import express from 'express';
-// import cors from 'cors';
-// // import { ApolloServer, gql } from 'apollo-server';
-// import { ApolloServer } from '@apollo/server';
-// import Account from '../infrastructure/models/account.js'; // Adjust the path as necessary
-// import connectToMongoDB from '../infrastructure/seeders/connectMongo.js'; // Adjust path as necessary
-// import resolvers from './resolvers.js';
-// import { buildSubgraphSchema } from '@apollo/subgraph';
-// const typeDefs = gql(readFileSync('./schema.graphql', { encoding: 'utf-8' }));
-
-
-// const startApolloServer = async () => {
-//   await connectToMongoDB();
-
-//   const server = new ApolloServer({
-//     schema:buildSubgraphSchema({ typeDefs, resolvers })
-//   });
-
-//   server.listen({ port: 4001 }).then(({ url }) => {
-//     console.log(`🚀 Server ready at ${url}`);
-//   });
-// };
-
-// startApolloServer();
 
