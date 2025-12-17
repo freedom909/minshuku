34f5cae36e675aaa9d43f73a551b4a604e0adaec
freedom909 <gifu0@outlook.com>
2025-12-17 16:00:45 +0900

    facial recognition startup complete

--- machine/facial-recognition/app.py
+++ machine/facial-recognition/app.py
@@ -5,8 +5,15 @@ from insightface.app import FaceAnalysis
 
 app = FastAPI()
 
-face_app = FaceAnalysis(name="buffalo_l", providers=["CPUExecutionProvider"])
-face_app.prepare(ctx_id=0, det_size=(640, 640))
+from insightface.app import FaceAnalysis
+
+face_app = FaceAnalysis(
+    name="buffalo_l",
+    root="/models",
+    providers=["CPUExecutionProvider"]
+)
+face_app.prepare(ctx_id=0)
+# face_app.prepare(ctx_id=0, det_size=(640, 640))
 
 
 def read_image(file: UploadFile):
